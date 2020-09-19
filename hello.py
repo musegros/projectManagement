2645e234c221a9fa8351dbd9f411bdeb120955ea
Josh Musgrave <musgrjaa@uni.edu>
2020-09-19 12:54:53 -0500

    I had to indent hello.py with spaces instead of tabs so that I could commit, but I forgot to add my changes to hello.py to the commit so now I have to do another commit

--- hello.py
+++ hello.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 def coin_toss( ):
-	    return ['Heads', 'Tails'][random.randint(0,1)]
+    return ['Heads', 'Tails'][random.randint(0,1)]
 
 @app.route("/")
 def hello():
-	return "Hello, World!"
+    return "Hello, World!"
